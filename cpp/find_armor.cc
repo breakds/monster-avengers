fce200d4e7a3581eca64b476cffb5267bbad8fc5
Break Yang <BreakDS@gmail.com>
2015-01-24 09:16:14 -0500

    add data parameter to find_armor

--- cpp/find_armor.cc
+++ cpp/find_armor.cc
@@ -7,13 +7,13 @@ using namespace monster_avengers;
 
 int main(int argc, char **argv) {
   std::setlocale(LC_ALL, "en_US.UTF-8");
-  ArmorUp armor_up("MH4G/");
-  
-  if (argc < 2) {
+  CHECK(2 <= argc);
+  ArmorUp armor_up(argv[1]);
+  if (argc < 3) {
     armor_up.ListSkills();
   } else {
     Query query;
-    CHECK_SUCCESS(Query::ParseFile(argv[1], &query));
+    CHECK_SUCCESS(Query::ParseFile(argv[2], &query));
     armor_up.SearchAndOutput(query, 10);
   }
   return 0;
