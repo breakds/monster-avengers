fefdd4950d5b6cb6536485a5f80a4209c6d81d00
Break Yang <BreakDS@gmail.com>
2015-06-20 17:22:32 -0400

    remove dataset

--- dataset/TESTING/cuirasses.lisp
+++
@@ -1,3 +0,0 @@
-
-((:EFFECTIVE-POINTS (3 -2 1) :EFFECTIVE-SKILLS ("加護" "スタミナ" "剣術") :HOLES 1
-  :DEFENSE 26 :RANK 3 :NAME "凛【胸当て】" :TYPE "melee" :OBJ T))
--- dataset/TORSO_UP/cuirasses.lisp
+++
@@ -1,3 +0,0 @@
-((:MATERIAL ("ランポスの剛爪" "黒狼鳥の剛翼" "黒狼鳥の重殻" "黒狼鳥の厚鱗") :EFFECTIVE-POINTS (2 5 2 -1)
-            :EFFECTIVE-SKILLS ("斬れ味" "聴覚保護" "達人" "剣術") :HOLES 2 :DEFENSE 142 :RANK 9
-            :NAME "ガルルガXメイル" :TYPE "melee" :OBJ T))
