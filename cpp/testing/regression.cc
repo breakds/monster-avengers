63cc43a0666039e1e7cc54491e90bff7e00f5f5c
BreakDS <breakds@gmail.com>
2015-08-03 15:08:03 -0400

    minor change to regression.cc

--- cpp/testing/regression.cc
+++ cpp/testing/regression.cc
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 
 #include "core/armor_up.h"
@@ -10,6 +11,32 @@ using namespace monster_avengers;
 using namespace monster_avengers::testing;
 
 using dataset::Data;
+
+// ---------- Utilities ----------
+
+std::string GetTestName(char *filename) {
+  int slash_pos = 0;
+  int dot_pos = 0;
+  int i = 0;
+  while (filename[i] != 0) {
+    if ('/' == filename[i]) {
+      slash_pos = i;
+    }
+    if ('.' == filename[i]) {
+      dot_pos = i;
+    }
+    ++i;
+  }
+
+  // Initialize test_name with spaces.
+  std::string test_name(dot_pos - slash_pos - 1, ' ');
+
+  i = slash_pos;
+  for (char &character : test_name) character = filename[++i];
+
+  return test_name;
+}
+
 
 // Arguments:
 // 1 - data set location
@@ -24,6 +51,9 @@ int main(int argc, char **argv) {
   // Load Data set
   Data::LoadSQLite(argv[1]);
 
+  // Prepare Timer
+  Timer timer;
+
   // Build Search Engine
   ArmorUp armor_up;
 
@@ -31,8 +61,19 @@ int main(int argc, char **argv) {
 
   for (int i = 2; i < argc; ++i) {
     CHECK_SUCCESS(Query::ParseFile(argv[i], &query));
-    
+
+    std::string test_name = GetTestName(argv[i]);
+    wprintf(L"+--------------------------------------+\n");
+    wprintf(L"|          Test: %s", test_name.c_str());
+    std::string padding(22 - test_name.size(), ' ');
+    wprintf(L"%s|\n", padding.c_str());
+    wprintf(L"+--------------------------------------+\n");
+
+    timer.Tic();
     std::vector<ArmorSet> result = std::move(armor_up.Search(query));
+    double duration = timer.Toc();
+    Log(INFO, L"Time elapsed for query: %.4lf seconds.", duration);
+
 
     int num_passed = 0;
     for (const ArmorSet &armor_set : result) {
@@ -43,7 +84,7 @@ int main(int argc, char **argv) {
         continue;
       }
 
-      wprintf(L"\n------------------------------------------------------------\n");
+      wprintf(L"\n");
       Data::PrintArmorSet(armor_set, armor_up.GetArsenal(), 1, CHINESE);
 
       for (const VerificationMessage &message : errors) {
